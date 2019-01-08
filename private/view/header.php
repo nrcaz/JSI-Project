2c8f4e9b89b326e13b8ae05d6c96cffa17315eb0
Cowcowricow <chipster_sk8eur@hotmail.com>
2019-01-09 00:26:38 +0100

    nav + logo

--- private/view/header.php
+++ private/view/header.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>JSI Project</title>
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
-    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/lux/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    
-    <header>
-        <h1>Le header comprenant</h1>
-        <ul>
-            <li>
-                <nav>
-                    <a href="index.php">Logo</a>
-                    <a href="services.php">Services</a>
-                    <a href="recherche.php">Recherche</a>
-                    <a href="contact.php">Contact</a>
-                </nav>
-                
-            </li>
-        </ul>
-    </header>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>JSI Project</title>
+        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
+        <link href="https://bootswatch.com/4/lux/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/nicho.css">
+    </head>
+
+    <body>
+        <header class="bg-dark">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <a class="navbar-brand" href="#"><img src="assets/Logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="#">Services <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">Recherche</a>
+                        <a class="nav-item nav-link" href="#">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
