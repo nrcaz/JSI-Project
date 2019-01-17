c0bab0f5c3c7135e6907318a22c60f293a48552c
Nicholas <nicholas.rossette@gmail.com>
2019-01-17 10:53:35 +0100

    index update

--- services.php
+++ services.php
@@ -1,4 +1,4 @@
 <?php
-    require_once("private/view/header.php");
-    require_once("private/view/sections/section-services.php");
-    require_once("private/view/footer.php");
+require_once "private/view/header.php";
+require_once "private/view/sections/section-services.php";
+require_once "private/view/footer.php";
