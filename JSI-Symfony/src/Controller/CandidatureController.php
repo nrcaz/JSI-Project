612fdd7ee545e3c02a3ee04544b3752be4791dc1
Nicholas <nicholas.rossette@gmail.com>
2019-01-22 15:35:57 +0100

    update

--- JSI-Symfony/src/Controller/CandidatureController.php
+++ JSI-Symfony/src/Controller/CandidatureController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Candidature;
 use App\Repository\CandidatureRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,5 +31,18 @@ class CandidatureController extends AbstractController
         return $this->render('candidature/show.html.twig', [
             'candidature' => $candidature,
         ]);
+    }
+    /**
+     * @Route("/admin/candidature/{id}", name="candidature_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Candidature $candidature): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$candidature->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($candidature);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('candidature');
     }
 }
