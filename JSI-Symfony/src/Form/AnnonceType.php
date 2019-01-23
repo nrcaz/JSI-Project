3e98b789f528aae9c4e0064a7174453e8db3fdb5
florian <florian.laffargue.laposte.net>
2019-01-23 18:06:51 +0100

    Mise à jour 18:06

--- JSI-Symfony/src/Form/AnnonceType.php
+++ JSI-Symfony/src/Form/AnnonceType.php
@@ -9,6 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -27,12 +28,16 @@ class AnnonceType extends AbstractType
             ->add('surface', NumberType::class)
             ->add('loyer', NumberType::class)
             ->add('charges', NumberType::class)
-            ->add('disponibilite', TextType::class)
+            ->add('disponibilite', ChoiceType::class, [
+                    'choices' => ['Oui' => 'Oui', 'Non' => 'Non'],
+                    ])
             ->add('bureaux', NumberType::class)
             ->add('openSpace', NumberType::class)
             ->add('salleReunion', NumberType::class)
             ->add('espaceDetente', NumberType::class)
-            ->add('accueil',TextType::class)
+            ->add('accueil',ChoiceType::class, [
+                    'choices' => ['Non' => 'Non', 'Oui' => 'Oui'],
+                    ])
            // ->add('dateCreation')
             ->add('note')
             ->add('image1Upload',FileType::class, [
