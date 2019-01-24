6f98583695348a97c72cb98684bd7682855b2892
Stephane <srouek@gmail.com>
2019-01-24 17:16:35 +0100

    ajout gif bouton modal

--- JSI-Symfony/public/assets/js/nicho.js
+++ JSI-Symfony/public/assets/js/nicho.js
@@ -3,15 +3,15 @@
 /////////////////////
 
 $("header .nav-link").on("click", function() {
-  console.log(screen.width + "" + innerWidth);
-  $("header .nav-link").css({
-    borderBottom: "solid 5px transparent"
-  });
-  if (screen.width > 990 && innerWidth > 990) {
-    $(this).css({
-      borderBottom: "solid 5px orange"
+    console.log(screen.width + "" + innerWidth);
+    $("header .nav-link").css({
+        borderBottom: "solid 5px transparent"
     });
-  }
+    if (screen.width > 990 && innerWidth > 990) {
+        $(this).css({
+            borderBottom: "solid 5px orange"
+        });
+    }
 });
 
 /////////////////////
@@ -20,17 +20,17 @@ $("header .nav-link").on("click", function() {
 
 // parallax
 $(".home-bg").parallax({
-  imageSrc: "assets/img/city.gif",
-  speed: 0.5
+    imageSrc: "assets/img/city.gif",
+    speed: 0.5
 });
 
 $(".parallax1").parallax({
-  imageSrc: "assets/img/background2.jpg",
-  speed: 0.3
+    imageSrc: "assets/img/background2.jpg",
+    speed: 0.3
 });
 $(".parallax2").parallax({
-  imageSrc: "assets/img/background2.jpg",
-  speed: 0.3
+    imageSrc: "assets/img/background2.jpg",
+    speed: 0.3
 });
 
 /////////////////////
@@ -39,34 +39,34 @@ $(".parallax2").parallax({
 
 // hover
 $("footer .nav-link").on("mouseover", function() {
-  $("footer .nav-link").css({
-    color: "white"
-  });
-  $(this).css({
-    color: "orange"
-  });
+    $("footer .nav-link").css({
+        color: "white"
+    });
+    $(this).css({
+        color: "orange"
+    });
 });
 $("footer .nav-link").on("mouseleave", function() {
-  $("footer .nav-link").css({
-    color: "white"
-  });
+    $("footer .nav-link").css({
+        color: "white"
+    });
 });
 
 // click on references
 let clickReferences = false;
 $('[href="#references"]').on("click", function(e) {
-  e.preventDefault();
-  if (!clickReferences) {
-    clickReferences = true;
-    $("#references").animate({
-      height: $("#references img").height()
-    });
-  } else {
-    clickReferences = false;
-    $("#references").animate({
-      height: 0
-    });
-  }
+    e.preventDefault();
+    if (!clickReferences) {
+        clickReferences = true;
+        $("#references").animate({
+            height: $("#references img").height()
+        });
+    } else {
+        clickReferences = false;
+        $("#references").animate({
+            height: 0
+        });
+    }
 });
 
 /////////////////////
@@ -78,42 +78,48 @@ let formSim = document.querySelector("#simulation");
 let annonces = [];
 
 if (formSim) {
-  formSim.addEventListener("submit", function(e) {
-    e.preventDefault();
-    // scroll to result
-    document.querySelector("#annonces").scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    });
+    formSim.addEventListener("submit", function(e) {
+        e.preventDefault();
+        // scroll to result
+        document.querySelector("#annonces").scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
 
-    // prepare AJAX
-    let formData = new FormData(formSim);
+        // prepare AJAX
+        let formData = new FormData(formSim);
 
-    let fetchOption = {
-      method: "POST",
-      body: formData
-    };
+        let fetchOption = {
+            method: "POST",
+            body: formData
+        };
 
-    fetch("http://localhost/jsi-project/jsi-symfony/public/ajax", fetchOption)
-      .then(response => response.json())
-      .then(response => {
-        // message
-        if (response.message)
-          document.querySelector("#message").textContent = response.message;
-        else
-          document.querySelector("#message").textContent =
-            "Les annonces correspondantes à votre recherche";
+        fetch(
+                "http://localhost/jsi-project/jsi-symfony/public/ajax",
+                fetchOption
+            )
+            .then(response => response.json())
+            .then(response => {
+                // message
+                if (response.message)
+                    document.querySelector("#message").textContent =
+                    response.message;
+                else
+                    document.querySelector("#message").textContent =
+                    "Les annonces correspondantes à votre recherche";
 
-        // liste annonce
-        annonces = response.annonces;
+                // liste annonce
+                annonces = response.annonces;
 
-        let html = "";
-        for (annonce of annonces) {
-          html += `
+                let html = "";
+                for (annonce of annonces) {
+                    html += `
 				<article class="card">
 					<figure><img src="${
-            annonce.image1 ? annonce.image1 : "assets/img/selection.jpg"
-          }" class="card-img-top" alt=""></figure>
+                        annonce.image1
+                            ? annonce.image1
+                            : "assets/img/selection.jpg"
+                    }" class="card-img-top" alt=""></figure>
 					<figcaption class="card-body">
 						<h5 class="card-title">${annonce.titre}</h5>
 						<p class="card-text">${annonce.description}</p>
@@ -121,27 +127,27 @@ if (formSim) {
 					</figcaption>
 				</article>
 				`;
-        }
-        document.querySelector(".annonceList").innerHTML = html;
+                }
+                document.querySelector(".annonceList").innerHTML = html;
 
-        let demande = `
+                let demande = `
 			Surface ${document.querySelector("#surfaceMin").value} - ${
-          document.querySelector("#surfaceMax").value
-        }
+                    document.querySelector("#surfaceMax").value
+                }
 			| ${document.querySelector("#nbBureaux").value}+ Bureaux
 			| ${document.querySelector("#nbOpenSpace").value}+ Open Space
 			| ${document.querySelector("#nbSalleReunion").value}+ Salles de Reunion
 			| ${document.querySelector("#nbEspaceDetente").value}+ Cuisine / Espace detente
 			`;
 
-        document.querySelector("#contact_demande").value = demande;
+                document.querySelector("#contact_demande").value = demande;
 
-        document.querySelector("#annonces").scrollIntoView({
-          behavior: "smooth",
-          block: "start"
-        });
-      });
-  });
+                document.querySelector("#annonces").scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+            });
+    });
 }
 
 ////////////////////////////
@@ -149,32 +155,32 @@ if (formSim) {
 ////////////////////////////
 // adjust height so footer is at bottom of page
 if (
-  innerHeight >
-  $("header").height() + $("footer").height() + $("#forgetpw").height()
+    innerHeight >
+    $("header").height() + $("footer").height() + $("#forgetpw").height()
 ) {
-  $("#forgetpw").css({
-    height: innerHeight - $("header").height() - $("footer").height()
-  });
+    $("#forgetpw").css({
+        height: innerHeight - $("header").height() - $("footer").height()
+    });
 }
 ////////////////////////////
 //// LOGIN
 ////////////////////////////
 if (
-  innerHeight >
-  $("header").height() + $("footer").height() + $("#login").height()
+    innerHeight >
+    $("header").height() + $("footer").height() + $("#login").height()
 ) {
-  $("#login").css({
-    height: innerHeight - $("header").height() - $("footer").height()
-  });
+    $("#login").css({
+        height: innerHeight - $("header").height() - $("footer").height()
+    });
 }
 ////////////////////////////
 //// CONFIRMATION
 ////////////////////////////
 if (
-  innerHeight >
-  $("header").height() + $("footer").height() + $("#confirmation").height()
+    innerHeight >
+    $("header").height() + $("footer").height() + $("#confirmation").height()
 ) {
-  $("#confirmation").css({
-    height: innerHeight - $("header").height() - $("footer").height()
-  });
+    $("#confirmation").css({
+        height: innerHeight - $("header").height() - $("footer").height()
+    });
 }
