4ed2c009454c3505a10d649277ad011818252d29
florian <florian.laffargue.laposte.net>
2019-01-25 10:45:24 +0100

    modif home

--- JSI-Symfony/public/assets/js/flo.js
+++ JSI-Symfony/public/assets/js/flo.js
@@ -1,15 +1,14 @@
 // Smooth scroll from Florian
 $(function() {
-  $("a[href='#services'], a[href='#commercialisation'], a[href='#contact']").on(
-    "click",
-    function(e) {
-      e.preventDefault();
+  $(
+    "a[href='#services'], a[href='#commercialisation'], a[href='#contact_home']"
+  ).on("click", function(e) {
+    e.preventDefault();
 
-      let hash = this.hash;
-      let hauteurNav = $("header").height();
-      let decalage = $(hash).offset().top;
+    let hash = this.hash;
+    let hauteurNav = $("header").height();
+    let decalage = $(hash).offset().top;
 
-      window.scrollTo({ top: decalage - hauteurNav, behavior: "smooth" });
-    }
-  );
+    window.scrollTo({ top: decalage - hauteurNav, behavior: "smooth" });
+  });
 });
