6bf243303930497968f156e34474575bec5cb65b
Nicholas <nicholas.rossette@gmail.com>
2019-01-29 15:38:04 +0100

    recherche section

--- JSI-Symfony/public/assets/js/nicho.js
+++ JSI-Symfony/public/assets/js/nicho.js
@@ -3,23 +3,23 @@
 /////////////////////
 
 if (innerHeight - $('header').height() > 650) {
-    $('#home').css({
-        height: innerHeight - $('header').height()
-    })
+	$('#home').css({
+		height : innerHeight - $('header').height()
+	});
 }
 // parallax
-$(".home-bg").parallax({
-    imageSrc: "assets/img/city.gif",
-    speed: 1,
+$('.home-bg').parallax({
+	imageSrc : 'assets/img/city.gif',
+	speed    : 1
 });
 
 $('.parallax1').parallax({
-    imageSrc: 'assets/img/background2.jpg',
-    speed: 0.3
+	imageSrc : 'assets/img/background2.jpg',
+	speed    : 0.3
 });
 $('.parallax2').parallax({
-    imageSrc: 'assets/img/background2.jpg',
-    speed: 0.3
+	imageSrc : 'assets/img/background2.jpg',
+	speed    : 0.3
 });
 
 /////////////////////
@@ -28,34 +28,34 @@ $('.parallax2').parallax({
 
 // hover
 $('footer .nav-link').on('mouseover', function() {
-    $('footer .nav-link').css({
-        color: 'white'
-    });
-    $(this).css({
-        color: 'orange'
-    });
+	$('footer .nav-link').css({
+		color : 'white'
+	});
+	$(this).css({
+		color : 'orange'
+	});
 });
 $('footer .nav-link').on('mouseleave', function() {
-    $('footer .nav-link').css({
-        color: 'white'
-    });
+	$('footer .nav-link').css({
+		color : 'white'
+	});
 });
 
 // click on references
 let clickReferences = false;
 $('[href="#references"]').on('click', function(e) {
-    e.preventDefault();
-    if (!clickReferences) {
-        clickReferences = true;
-        $('#references').animate({
-            height: $('#references img').height()
-        });
-    } else {
-        clickReferences = false;
-        $('#references').animate({
-            height: 0
-        });
-    }
+	e.preventDefault();
+	if (!clickReferences) {
+		clickReferences = true;
+		$('#references').animate({
+			height : $('#references img').height()
+		});
+	} else {
+		clickReferences = false;
+		$('#references').animate({
+			height : 0
+		});
+	}
 });
 
 /////////////////////
@@ -64,104 +64,108 @@ $('[href="#references"]').on('click', function(e) {
 // MODAL
 // height of collapse-modal
 $('#collapse-modal').css({
-    height: $('#btn-modal').height() +
-        parseInt($('#btn-modal').css('paddingTop')) +
-        parseInt($('#btn-modal').css('paddingBottom'))
+	height :
+		$('#btn-modal').height() +
+		parseInt($('#btn-modal').css('paddingTop')) +
+		parseInt($('#btn-modal').css('paddingBottom'))
 });
 let divModalWidth =
-    $('#div-modal').width() +
-    parseInt($('#div-modal').css('paddingLeft')) +
-    parseInt($('#div-modal').css('paddingRight'));
+	$('#div-modal').width() +
+	parseInt($('#div-modal').css('paddingLeft')) +
+	parseInt($('#div-modal').css('paddingRight'));
 let btnModalWidth =
-    $('#btn-modal').width() +
-    parseInt($('#btn-modal').css('paddingLeft')) +
-    parseInt($('#btn-modal').css('paddingRight'));
+	$('#btn-modal').width() +
+	parseInt($('#btn-modal').css('paddingLeft')) +
+	parseInt($('#btn-modal').css('paddingRight'));
 
 // position out of screen
 $('#div-modal').css({
-    top: innerHeight / 2 - $('#div-modal').height() / 2,
-    left: -divModalWidth
+	top  : innerHeight / 2 - $('#div-modal').height() / 2,
+	left : -divModalWidth
 });
 let collapse = true;
 
 function modalPop() {
-    if (collapse) {
-        // animation on submit
-        document
-            .querySelector('#div-modal')
-            .animate([{ transform: 'translateX(0px)' }, { transform: `translateX(${divModalWidth}px)` }], {
-                duration: 300
-            }).onfinish = function() {
-                $('#div-modal').css({
-                    left: 0
-                });
-            };
-        collapse = false;
-    }
+	if (collapse) {
+		// animation on submit
+		document.querySelector('#div-modal').animate([
+			{ transform: 'translateX(0px)' },
+			{ transform: `translateX(${divModalWidth}px)` }
+		], {
+			duration : 300
+		}).onfinish = function() {
+			$('#div-modal').css({
+				left : 0
+			});
+		};
+		collapse = false;
+	}
 
-    document.querySelector('#collapse-modal').addEventListener('click', function(e) {
-        e.preventDefault();
-        if (!collapse) {
-            // animation collapse
-            document
-                .querySelector('#div-modal')
-                .animate([{ transform: 'translateX(0px)' }, { transform: `translateX(${-btnModalWidth}px)` }], {
-                    duration: 300
-                }).onfinish = function() {
-                    $('#div-modal').css({
-                        left: -btnModalWidth
-                    });
-                    $('#collapse-modal').html('<i class="fas fa-chevron-right"></i>');
-                    collapse = true;
-                };
-        } else {
-            // animation uncollapse
-            document
-                .querySelector('#div-modal')
-                .animate([{ transform: 'translateX(0px)' }, { transform: `translateX(${btnModalWidth}px)` }], {
-                    duration: 300
-                }).onfinish = function() {
-                    $('#div-modal').css({
-                        left: 0
-                    });
-                    $('#collapse-modal').html('<i class="fas fa-chevron-left"></i>');
-                    collapse = false;
-                };
-        }
-    });
+	document.querySelector('#collapse-modal').addEventListener('click', function(e) {
+		e.preventDefault();
+		if (!collapse) {
+			// animation collapse
+			document.querySelector('#div-modal').animate([
+				{ transform: 'translateX(0px)' },
+				{ transform: `translateX(${-btnModalWidth}px)` }
+			], {
+				duration : 300
+			}).onfinish = function() {
+				$('#div-modal').css({
+					left : -btnModalWidth
+				});
+				$('#collapse-modal').html('<i class="fas fa-chevron-right"></i>');
+				collapse = true;
+			};
+		} else {
+			// animation uncollapse
+			document.querySelector('#div-modal').animate([
+				{ transform: 'translateX(0px)' },
+				{ transform: `translateX(${btnModalWidth}px)` }
+			], {
+				duration : 300
+			}).onfinish = function() {
+				$('#div-modal').css({
+					left : 0
+				});
+				$('#collapse-modal').html('<i class="fas fa-chevron-left"></i>');
+				collapse = false;
+			};
+		}
+	});
 }
 
 // AJAX
 let formSim = document.querySelector('#simulation');
 let annonces = [];
 if (formSim) {
-    formSim.addEventListener('submit', function(e) {
-        e.preventDefault();
-        // scroll to result
-        document.querySelector('#annonces').scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+	formSim.addEventListener('submit', function(e) {
+		e.preventDefault();
+		// scroll to result
+		document.querySelector('#annonces').scrollIntoView({
+			behavior : 'smooth',
+			block    : 'start'
+		});
 
-        // prepare AJAX
-        let formData = new FormData(formSim);
+		// prepare AJAX
+		let formData = new FormData(formSim);
 
-        let fetchOption = {
-            method: 'POST',
-            body: formData
-        };
+		let fetchOption = {
+			method : 'POST',
+			body   : formData
+		};
 
-        fetch(window.pathAjax, fetchOption).then((response) => response.json()).then((response) => {
-            // message
-            if (response.message) document.querySelector('#message').textContent = response.message;
-            else document.querySelector('#message').textContent = 'Les annonces correspondantes à votre recherche';
+		fetch(window.pathAjax, fetchOption).then(response => response.json()).then(response => {
+			// message
+			if (response.message) document.querySelector('#message').textContent = response.message;
+			else document.querySelector('#message').textContent = 'Les annonces correspondantes à votre recherche';
 
-            // liste annonce
-            annonces = response.annonces;
+			// liste annonce
+			annonces = response.annonces;
 
-            let html = '';
-            for (annonce of annonces) {
-                html += `
+			let html = '';
+			for (annonce of annonces) {
+				html += `
 				<article class="card">
 					<figure><img src="${annonce.image1
 						? annonce.image1
@@ -173,20 +177,30 @@ if (formSim) {
 					</figcaption>
 				</article>
 				`;
-                }
-                document.querySelector("#annonceList").innerHTML = html;
+			}
+			document.querySelector('#annonceList').innerHTML = html;
 
-                let demande = `Surface ${document.querySelector("#surfaceMin").value} - ${document.querySelector("#surfaceMax").value} m² \n| ${document.querySelector("#nbBureaux").value ? document.querySelector("#nbBureaux").value : "0"}+ Bureaux \n| ${document.querySelector("#nbOpenSpace").value ? document.querySelector("#nbOpenSpace").value : "0"}+ Open Space \n| ${document.querySelector("#nbSalleReunion").value ? document.querySelector("#nbSalleReunion").value : "0"}+ Salles de Reunion \n| ${document.querySelector("#nbEspaceDetente").value ? document.querySelector("#nbEspaceDetente").value : "0"}+ Cuisine / Espace detente`;
+			let demande = `Surface ${document.querySelector('#surfaceMin').value} - ${document.querySelector(
+				'#surfaceMax'
+			).value} m² \n| ${document.querySelector('#nbBureaux').value
+				? document.querySelector('#nbBureaux').value
+				: '0'}+ Bureaux \n| ${document.querySelector('#nbOpenSpace').value
+				? document.querySelector('#nbOpenSpace').value
+				: '0'}+ Open Space \n| ${document.querySelector('#nbSalleReunion').value
+				? document.querySelector('#nbSalleReunion').value
+				: '0'}+ Salles de Reunion \n| ${document.querySelector('#nbEspaceDetente').value
+				? document.querySelector('#nbEspaceDetente').value
+				: '0'}+ Cuisine / Espace detente`;
 
-            document.querySelector('#contact_demande').value = demande;
+			document.querySelector('#contact_demande').value = demande;
 
-            document.querySelector('#annonces').scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-            modalPop();
-        });
-    });
+			document.querySelector('#annonces').scrollIntoView({
+				behavior : 'smooth',
+				block    : 'start'
+			});
+			modalPop();
+		});
+	});
 }
 
 ////////////////////////////
@@ -194,25 +208,25 @@ if (formSim) {
 ////////////////////////////
 // adjust height so footer is at bottom of page
 if (innerHeight > $('header').height() + $('footer').height() + $('#forgetpw').height()) {
-    $('#forgetpw').css({
-        height: innerHeight - $('header').height() - $('footer').height()
-    });
+	$('#forgetpw').css({
+		height : innerHeight - $('header').height() - $('footer').height()
+	});
 }
 ////////////////////////////
 //// LOGIN
 ////////////////////////////
 if (innerHeight > $('header').height() + $('footer').height() + $('#login').height()) {
-    $('#login').css({
-        height: innerHeight - $('header').height() - $('footer').height()
-    });
+	$('#login').css({
+		height : innerHeight - $('header').height() - $('footer').height()
+	});
 }
 ////////////////////////////
 //// CONFIRMATION
 ////////////////////////////
 if (innerHeight > $('header').height() + $('footer').height() + $('#confirmation').height()) {
-    $('#confirmation').css({
-        height: innerHeight - $('header').height() - $('footer').height()
-    });
+	$('#confirmation').css({
+		height : innerHeight - $('header').height() - $('footer').height()
+	});
 }
 
 ////////////////////////////
@@ -220,9 +234,9 @@ if (innerHeight > $('header').height() + $('footer').height() + $('#confirmation
 ////////////////////////////
 
 $(document).ready(function() {
-    $('[data-toggle="popover"]').popover({
-        html: true,
-        trigger: 'focus',
-        imageSrc: 'assets/img/shorlist.jpg'
-    });
+	$('[data-toggle="popover"]').popover({
+		html     : true,
+		trigger  : 'focus',
+		imageSrc : 'assets/img/shorlist.jpg'
+	});
 });
